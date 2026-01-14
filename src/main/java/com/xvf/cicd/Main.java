41db5e63da8be53cdd1ac943b615c6185ed399f8
Xavi <xavier.valverde926@comunidadunir.net>
2026-01-14 13:00:04 +0100

    UPDATE: Fallo aposta en compilacion para verificar errores en CI

--- src/main/java/com/xvf/cicd/Main.java
+++ src/main/java/com/xvf/cicd/Main.java
@@ -11,9 +11,7 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-
-            System.out.println("La versión actual es: " + System.getProperty("java.version"));
+            System.out.println("i = " + i)
         }
     }
 }
